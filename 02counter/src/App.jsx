0f83d4a8f89c9e5b85fe7bfe862dde38a4b7a477
Bobby <learnings.bobby.0089@gmail.com>
2025-11-30 20:04:02 +0530

    Why we need hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+  /*
+  When your component refreshes, normal variables forget their values.
+  useState helps your component remember things. 
+
+  It gives two things:
+  const [value, setValue] = useState(startValue);
+
+  value → the current data
+  setValue → a function to change that data
+  */
+  
+  // let counter = 5;
+
+  function addValue(){
+    // counter++;
+        
+    /*
+    counter++ changes the value, but the UI does not update.
+    React only updates the screen when state changes.
+    That’s why we must use hooks (like useState) to update the UI.
+    */
+    // console.log(counter);
+
+    //calling the function
+    counter = counter + 1
+    setCounter(counter)
+  }
+
+  function decreaseValue(){
+    counter = counter - 1;
+    setCounter(counter)
+  }
+
+  return (
+    <>
+      <h1>React</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button 
+      onClick={addValue}
+      >Increase value</button><br /><br />
+      <button
+      onClick={decreaseValue}
+      >Decrease value</button>
+    </>
+  )
+}
+
+export default App
