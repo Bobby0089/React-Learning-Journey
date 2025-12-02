6b1eba1a91b963e4a715fe202be95bde8a3a5a3d
Bobby <learnings.bobby.0089@gmail.com>
2025-12-02 10:37:37 +0530

    Add currency exchange project using custom hooks

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+
+    useEffect( () => {
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+        .then( (response) => response.json())
+        .then( (res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+
+    return data;  
+}
+
+export default useCurrencyInfo;
