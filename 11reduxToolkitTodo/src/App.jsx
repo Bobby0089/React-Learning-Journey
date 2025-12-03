ed233db03a19c7bb4c6848e35e16c56d444ba154
Bobby <learnings.bobby.0089@gmail.com>
2025-12-03 14:09:06 +0530

    redux toolkit project

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
